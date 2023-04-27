6727d1a7c61d37d74665c922416e66669f1f2a7f
ngimdock <ngimdockzemfack@gmail.com>
2023-04-27 11:44:40 +0100

    use repo getByid

--- src/challengeRepository.ts
+++ src/challengeRepository.ts
@@ -2,17 +2,23 @@ import { ChallengeRow, PrismaClient } from "@prisma/client";
 
 export type ChallengeRepository = {
   getAll: () => Promise<ChallengeRow[]>;
-  // getById: () => Promise<ChallengeRow>;
+  getById: (id: string) => Promise<ChallengeRow | null>;
   // create: () => Promise<string>;
   // delete: () => Promise<void>;
 };
 
-export const challengeRepositoryFactory = (prismaClient: PrismaClient) => {
+export const challengeRepositoryFactory = (
+  prismaClient: PrismaClient
+): ChallengeRepository => {
   return {
     getAll: () => {
       return prismaClient.challengeRow.findMany();
     },
-    // getById: () => {},
+    getById: (id: string) => {
+      return prismaClient.challengeRow.findUnique({
+        where: { id },
+      });
+    },
     // create: () => {},
     // delete: () => {},
   };
